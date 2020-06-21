68052d15e75f353adb5a891b90c05d07a48a430e
Artem <artemkurchakov1018@gmail.com>
2020-06-21 12:02:22 +0300

    some tests added

--- atm_test.py
+++ atm_test.py
@@ -7,21 +7,29 @@ class TestAtmNew(unittest.TestCase):
         self.atm = Atm()
         self.balance = 10000
         self.attempts = 2
-        # self.client_can_get_money = False
+        self.atm.client_can_get_money = True
 
 
     def test_rise_money(self):
-        self.assertEqual(self.atm.rise_money(0), 10000, "User can't add 0 money")
         self.assertEqual(self.atm.rise_money(500), 10500, "User can't add 500 money")
+
+    def test_rise_money_boundary_values(self):
         self.assertEqual(self.atm.rise_money(99999999999999999999), 100000000000000009999, "User can't add much money")
         self.assertEqual(self.atm.rise_money(0.01), 10000.01, "User can't add tight money")
+        self.assertEqual(self.atm.rise_money(0), 10000, "User can't add 0 money")
+
+    def test_rise_money_negative_meaning(self):
+        self.assertEqual(self.atm.rise_money(-1), 10000, "User can't rise negative meaning")
 
     def test_rise_money_twice(self):
-        self.first_add = self.atm.rise_money(300)
-        self.second_add = self.first_add + 400
+        first_add = self.atm.rise_money(300)
+        second_add = first_add + 400
         self.assertEqual(self.atm.rise_money(700), 10700, "User can't added money twice")
 
     def test_get_money(self):
+        self.assertEqual(self.atm.get_money(1000), 1000, "User can't get 1000 money")
+
+    def test_get_money_boundary_values(self):
         self.assertEqual(self.atm.get_money(0.01), 0.01, "User can't get tight money")
         self.assertEqual(self.atm.get_money(9999.99), 9999.99, "User can't get money")
         self.assertEqual(self.atm.get_money(0), 0, "User can't get zero money")
@@ -29,8 +37,33 @@ class TestAtmNew(unittest.TestCase):
     def test_negative_meaning(self):
         self.assertEqual(self.atm.get_money(-1), 0, "User can't get negative value")
 
+    def test_get_money_withdraw_more_money_than_on_the_account(self):
+        self.assertEqual(self.atm.get_money(10500), self.balance)
+
+    def test_get_money_withdraw_more_money_than_on_the_account_boundary_values(self):
+        self.assertEqual(self.atm.get_money(10000.01), self.balance)
+
     def test_check_balance(self):
-        self.assertFalse(self.atm.check_balance(), "User can't check balance")
+        self.assertEqual(self.atm.check_balance(), 10000, "User can't check balance")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
